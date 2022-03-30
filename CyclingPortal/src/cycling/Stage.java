605533b44dccfbdd2254b9c0b3978a23be0bedad
Maddie Hansford <madelene.hansford@gmail.com>
2022-03-30 16:44:03 +0100

    stage comments

--- CyclingPortal/src/cycling/Stage.java
+++ CyclingPortal/src/cycling/Stage.java
@@ -35,14 +35,16 @@ public class Stage  implements Serializable {
   /**
    * Constructor method that sets a Stage up with a race, name, description, length
    * startTime and type.
-   * @param Race race: Race that the Stage is in.
-   * @param String name: name of the Stage.
-   * @param String description: description of the Stage.
-   * @param double length:
-   * @param startTime
-   * @param type
-   * @throws InvalidNameException
-   * @throws InvalidLengthException
+   *
+   * @param race: Race that the Stage is in.
+   * @param name: name of the Stage.
+   * @param description: description of the Stage.
+   * @param length: length of the Stage.
+   * @param startTime: start time of the Stage.
+   * @param type: the type of Stage.
+   * @throws InvalidNameException Thrown if the name is empty, null, longer than 30
+   * characters or contains whitespace.
+   * @throws InvalidLengthException Thrown if the length is less than 5km.
    */
   public Stage(
       Race race,
@@ -68,51 +70,111 @@ public class Stage  implements Serializable {
     this.length = length;
     this.startTime = startTime;
     this.type = type;
+    // ID counter represents the highest known ID at the current time to ensure there
+    // are no ID collisions.
     this.id = Stage.count++;
   }
 
+  /** Method that resets the static ID counter of the Race. Used for erasing and loading. */
   static void resetIdCounter() {
     count = 0;
   }
 
+  /**
+   * Method to get the current state of the static ID counter.
+   *
+   * @return the highest race ID stored currently.
+   */
   static int getIdCounter() {
     return count;
   }
 
+  /**
+   * Method that sets the static ID counter to a given value. Used when loading to avoid ID
+   * collisions.
+   *
+   * @param newCount: new value of the static ID counter.
+   */
   static void setIdCounter(int newCount) {
     count = newCount;
   }
 
+  /**
+   * Method to get the ID of the Race object.
+   *
+   * @return id: the Race's unique ID value.
+   */
   public int getId() {
     return id;
   }
 
+  /**
+   * Method to get the name of the Stage.
+   *
+   * @return name: the given name of the Stage.
+   */
   public String getName() {
     return name;
   }
 
+  /**
+   * Method to get the length of the Stage.
+   * 
+   * @return length: the given length of the Stage.
+   */
   public double getLength() {
     return length;
   }
 
+  /**
+   * Method to get the Stage's Race.
+   * 
+   * @return race: the given Race that the Stage is in.
+   */
   public Race getRace() {
     return race;
   }
 
+  /**
+   * Method to get the Stage's type.
+   * 
+   * @return type: the given type of the Stage
+   */
   public StageType getType() {
     return type;
   }
 
+  /**
+   * Method to get the Segments in the Stage.
+   * 
+   * @return segments: a list of Segments in the Stage.
+   */
   public ArrayList<Segment> getSegments() {
     return segments;
   }
 
+  /**
+   * Method to get the start time of the Stage.
+   * 
+   * @return startTime: the given start time of the Stage.
+   */
   public LocalDateTime getStartTime() {
     return startTime;
   }
 
+  /**
+   * Method that adds a Segment to the Stage. It is added to the list of Segments
+   * based on its location in the Stage.
+   * 
+   * @param segment: Segment that will be added to the Stage.
+   */
   public void addSegment(Segment segment) {
+    // Loops through the ordered list of segments to find the correct place for the new
+    // Segment to be added.
     for (int i = 0; i < segments.size(); i++) {
+      // Compares the Segments based on their locations.
+      // The new Segment is inserted if its location is less than the location of the 
+      // current Segment it is being compared to.
       if (segment.getLocation() < segments.get(i).getLocation()) {
         segments.add(i, segment);
         return;
@@ -121,14 +183,31 @@ public class Stage  implements Serializable {
     segments.add(segment);
   }
 
+  /**
+   * Method that removes a given Segment from the Stage's Segments.
+   * 
+   * @param segment: the Segment intended to be removed.
+   * @throws InvalidStageStateException Thrown if the Stage is waiting for results.
+   */
   public void removeSegment(Segment segment) throws InvalidStageStateException {
     if (waitingForResults) {
       throw new InvalidStageStateException(
-          "The stage cannot be removed as it is waiting for results.");
+          "The segment cannot be removed as it is waiting for results.");
     }
     segments.remove(segment);
   }
 
+  /**
+   * Method that registers a Rider's result and adds it to the Stage.
+   * 
+   * @param rider: the Rider whose results will be registered.
+   * @param checkpoints: the Rider's results.
+   * @throws InvalidStageStateException Thrown if the Stage is not waiting for results.
+   * @throws DuplicatedResultException Thrown if the Rider already has results registered
+   * in the Stage.
+   * @throws InvalidCheckpointsException Thrown if the number checkpoints doesn't equal the
+   * number of Segments in the Stage + 2
+   */
   public void registerResult(Rider rider, LocalTime[] checkpoints)
       throws InvalidStageStateException, DuplicatedResultException, InvalidCheckpointsException {
     if (!waitingForResults) {
@@ -140,7 +219,7 @@ public class Stage  implements Serializable {
     }
     if (checkpoints.length != segments.size() + 2) {
       throw new InvalidCheckpointsException(
-          "The length of the checkpoint must equal number of Segments in the Stage + 2.");
+          "The length of the checkpoint must equal the number of Segments in the Stage + 2.");
     }
 
     StageResult result = new StageResult(checkpoints);
@@ -153,6 +232,12 @@ public class Stage  implements Serializable {
     }
   }
 
+  /**
+   * Method that concludes the Stage preparation and ensures that the Stage is now
+   * waiting for results.
+   * 
+   * @throws InvalidStageStateException Thrown if the Stage is already waiting for results.
+   */
   public void concludePreparation() throws InvalidStageStateException {
     if (waitingForResults) {
       throw new InvalidStageStateException("Stage is already waiting for results.");
@@ -160,19 +245,39 @@ public class Stage  implements Serializable {
     waitingForResults = true;
   }
 
+  /**
+   * Method to identify whether the Stage is waiting for results.
+   * 
+   * @return A boolean, true if the Stage is waiting for results, false if it is not.
+   */
   public boolean isWaitingForResults() {
     return waitingForResults;
   }
 
+  /**
+   * Method to calculate and return the results of a given Rider.
+   * 
+   * @param rider: Rider whose results are desired.
+   * @return results of the Rider.
+   */
   public StageResult getRiderResult(Rider rider) {
     calculateResults();
     return results.get(rider);
   }
 
+  /**
+   * Method to remove the results of a Rider.
+   * 
+   * @param rider whose results are to be removed.
+   */
   public void removeRiderResults(Rider rider) {
     results.remove(rider);
   }
 
+  /**
+   * Method to 
+   * @return
+   */
   public List<Rider> getRidersByElapsedTime() {
     calculateResults();
     return sortRiderResults();
